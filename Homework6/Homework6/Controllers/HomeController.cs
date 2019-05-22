c8bf965f742d8c746babc1aa19650ef55248040a
cat1116 <catherinelpowell@msn.com>
2019-05-22 00:21:22 -0400

    Class 6 Homework - Catherine Powell

---
+++ Homework6/Homework6/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendBirthdayCard()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SendBirthdayCard(Models.BirthdayCard birthdayCard)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("CardSent", birthdayCard);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
